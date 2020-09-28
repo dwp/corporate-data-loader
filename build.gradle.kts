ff53a95ee0ec9dd9b1a4c5f4e37a3a162687d3f8
Daniel Chicot <dan.chicot@gmail.com>
2020-09-28 09:44:59 +0100

    .gitignore, gradle files.

---
+++ build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    java
+    kotlin("jvm") version "1.4.10"
+    application
+    id("com.github.johnrengelman.shadow") version "5.1.0"
+}
+
+group = "corporate-data-loader"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("org.apache.hbase:hbase-client:1.4.13")
+    implementation("org.apache.hbase:hbase-server:1.4.13")
+}
+
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+}
+
+application {
+    mainClassName = "app.load.LoadKt"
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
